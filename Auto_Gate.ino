49d69b99d7197457080e6994d9e031264a974fb7
Himanshu495-rada <61615912+Himanshu495-rada@users.noreply.github.com>
2020-12-31 11:07:56 +0530

    Update Auto_Gate.ino

--- Auto_Gate.ino
+++ Auto_Gate.ino
@@ -7,12 +7,14 @@ WiFiServer server(80);
  
 const int relay_1 = 2;
 const int relay_2 = 4;
+const int relay_3 = 25;
+const int relay_4 = 33;
  
 void processReceivedValue(char command){
  
-  if(command == '1'){ digitalWrite(relay_1, LOW); }
-  else if(command == '2'){ digitalWrite(relay_2, LOW);}
-  else if(command == '0') {digitalWrite(relay_1, HIGH); digitalWrite(relay_2, HIGH);}
+  if(command == '1'){ digitalWrite(relay_1, LOW); digitalWrite(relay_2, LOW);}
+  else if(command == '2'){ digitalWrite(relay_3, LOW); digitalWrite(relay_4, LOW);}
+  else if(command == '0') {digitalWrite(relay_1, HIGH); digitalWrite(relay_2, HIGH); digitalWrite(relay_3, HIGH); digitalWrite(relay_4, HIGH);}
  
    return;
 }
@@ -39,6 +41,11 @@ void setup() {
   digitalWrite(relay_1, HIGH);
   pinMode(relay_2, OUTPUT);
   digitalWrite(relay_2, HIGH);
+  pinMode(relay_3, OUTPUT);
+  digitalWrite(relay_3, HIGH);
+  pinMode(relay_4, OUTPUT);
+  digitalWrite(relay_4, HIGH);
+
 }
  
 void loop() {
